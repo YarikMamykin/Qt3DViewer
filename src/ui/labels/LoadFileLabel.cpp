b377fe768d81be12024d065259a2bb01d3beec7f
YarikMamykin <ymamykin@gmail.com>
2020-11-23 01:22:10 +0200

    Update labels design

--- src/ui/labels/LoadFileLabel.cpp
+++ src/ui/labels/LoadFileLabel.cpp
@@ -1,13 +1,27 @@
 #include "ui/labels/LoadFileLabel.h"
 #include <QString>
+#include <QFont>
+#include <QStringList>
 
 namespace ui {
   namespace labels {
     LoadFileLabel::LoadFileLabel(const QString& name)
       : QLabel(name) { 
-        this->setFixedHeight(50);
-        this->setFixedWidth(300);
-        this->setStyleSheet("background-color:red;");
+        const QStringList styleSheet = {
+          "color: #FF8C00;", 
+          "background-color:black;",
+        };
+
+        this->setFixedWidth(310);
+        this->setStyleSheet(styleSheet.join(" "));
+
+        QFont font("Courier New");
+        font.setStyleHint(QFont::Monospace);
+        font.setPixelSize(15);
+        font.setBold(true);
+
+        this->setFont(font);
+        this->setFixedHeight(this->sizeHint().height());
       }
   }
 }
--- src/ui/labels/SelectedFileLabel.cpp
+++ src/ui/labels/SelectedFileLabel.cpp
@@ -4,9 +4,24 @@ namespace ui {
   namespace labels {
     SelectedFileLabel::SelectedFileLabel() 
       : QLabel() {
-        this->setFixedHeight(50);
-        this->setFixedWidth(300);
-        this->setStyleSheet("background-color:cyan;");
+        const QStringList styleSheet = {
+          "color: #C8C8C8;", 
+          "background-color:black;",
+          "border: 1px solid white;",
+          "border-color: gray;"
+        };
+
+        this->setFixedHeight(20);
+        this->setFixedWidth(310);
+        this->setStyleSheet(styleSheet.join(" "));
+
+        QFont font("Courier New");
+        font.setStyleHint(QFont::Monospace);
+        font.setPixelSize(12);
+        font.setBold(true);
+
+        this->setFont(font);
+        this->setFixedHeight(this->sizeHint().height());
       }
   }
 }
